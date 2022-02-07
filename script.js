27266d3d4edcb283ebcfafb7dc70f1abfe46c595
GiveMeaName0 <mikprono102@gmail.com>
2022-02-07 19:35:16 +0700

    update

--- script.js
+++ script.js
@@ -101,6 +101,19 @@ document.querySelectorAll('.products-container .product').forEach(product =>{
   };
 });
 
+document.querySelectorAll('.products-container--responsive .product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
+
 previewBox.forEach(close =>{
   close.querySelector('.close-btn').onclick = () =>{
     close.classList.remove('active');
